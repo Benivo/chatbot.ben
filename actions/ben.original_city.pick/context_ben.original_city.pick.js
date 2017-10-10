5e252e2a44276802eed75aff3846a201b499225b
DimaBenivo <dima@benivo.com>
2017-10-10 14:35:49 +0100

    automatic push

--- actions/ben.original_city.pick/context_ben.original_city.pick.js
+++ actions/ben.original_city.pick/context_ben.original_city.pick.js
@@ -12,13 +12,21 @@ module.exports = {
   {
     // set different languages 
     let original_country_data=context_common.get_original_country_data(requestBody,parameterscontextout);
-    parameterscontextout["user_language_Hello"]=original_country_data.Hello;
-    parameterscontextout["user_language_Goodbye"]=original_country_data.Goodbye;
-    parameterscontextout["user_language_Great"]=original_country_data.Great;
-    
-    parameterscontextout["original_capital"]=original_country_data.Capital_cities;
-
-    infographics.infographic_1(parameterscontextout,requestBody);
+    try{
+      parameterscontextout["user_language_Hello"]=original_country_data.Hello;
+      parameterscontextout["user_language_Goodbye"]=original_country_data.Goodbye;
+      parameterscontextout["user_language_Great"]=original_country_data.Great;    
+      parameterscontextout["original_capital"]=original_country_data.Capital_cities;    
+      if(original_country_data.Capital_cities!=undefined){
+        infographics.infographic_1(parameterscontextout,requestBody);
+      }
+    }
+    catch(err){
+      console.log(parameterscontextout["log_id"]+" no country parameters")
+      parameterscontextout["user_language_Hello"]="Hello";
+      parameterscontextout["user_language_Goodbye"]="Goodbye";
+      parameterscontextout["user_language_Great"]="Great";    
+    }
 
     
 
